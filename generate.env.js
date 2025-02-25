7cf53308acaf08343cd49941737034019ab34d40
Emiliano Rago <emilianorago.dev@gmail.com>
2025-02-25 02:25:54 -0300

    se agrega archivo de creacion de variables y se agrega un script

---
+++ generate.env.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+require('dotenv').config();
+
+// Verifica si la carpeta environments no existe, y si no, la crea
+if (!fs.existsSync('src/environments')) {
+  fs.mkdirSync('src/environments');
+}
+const {
+  API_URL,
+} = process.env;
+
+// Contenido de environment.ts
+const contentProd = `export const environment = {
+API_URL: '${API_URL}'
+};`;
+
+fs.writeFileSync('src/environments/environment.ts', contentProd);
